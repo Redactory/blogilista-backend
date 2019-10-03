14613a235a8dfa58238aef28771636d8ad845782
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-10-03 10:59:04 +0300

    Polkujen siirto omaan moduliin
    Tehtävän 4.2 toteutuksen viimeinen osa.

--- index.js
+++ index.js
@@ -6,29 +6,11 @@ const cors = require('cors');
 const server = http.createServer(app);
 
 const config = require('./utils/config');
-const Blog = require('./models/blog');
-
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    });
-});
+app.use('/api/blogs', blogsRouter);
 
 server.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`)    
