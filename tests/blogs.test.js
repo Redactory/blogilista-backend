e7c460992e2c9ac9560c66cb1eb5528179b4219e
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-10-03 15:55:13 +0300

    Tehtävän 4.4 totetus

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+const Blogs = require('./test_data');
 const listHelper = require('../utils/list_helper');
 
 test('dummy returns one', () => {
@@ -6,4 +7,21 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
+});
+
+describe('number of likes', () => {
+  test('when list has serveral blogs', () => {
+    const result = listHelper.totalLikes(Blogs.allBlogs);
+    expect(result).toBe(36);
+  });
+
+  test('when list has no blogs', () => {
+    const result = listHelper.totalLikes(Blogs.emptyBlogList);
+    expect(result).toBe(0);
+  });
+
+  test('when list has one blog', () => {
+    const result = listHelper.totalLikes(Blogs.oneBlogList);
+    expect(result).toBe(7);
+  });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,18 @@
+/* eslint-disable arrow-body-style */
+const reducer = (total, addition) => {
+  return { likes: total.likes + addition.likes };
+};
+
 const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  const result = blogs.reduce((total, addition) => reducer(total, addition), { likes: 0 });
+  return result.likes;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
