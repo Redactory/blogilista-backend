2ea81d848331ef67e56d06183d1d401d17e11f47
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-10-03 10:02:42 +0300

    Lisätään puolipisteitä

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const config = require('./utils/config');
 const server = http.createServer(app);
@@ -15,32 +15,32 @@ const blogSchema = mongoose.Schema({
   likes: Number
 });
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb+srv://Teemu:DBuser@puhelinluettelodb-hlufo.mongodb.net/bloglist?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl, { useNewUrlParser: true })
+const mongoUrl = 'mongodb+srv://Teemu:DBuser@puhelinluettelodb-hlufo.mongodb.net/bloglist?retryWrites=true&w=majority';
+mongoose.connect(mongoUrl, { useNewUrlParser: true });
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
       response.json(blogs)
-    })
-})
+    });
+});
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog(request.body);
 
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
-    })
-})
+    });
+});
 
 server.listen(config.PORT, () => {
     console.log(`Server running on port ${config.PORT}`)    
-})
+});
