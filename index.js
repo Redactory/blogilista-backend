cb6295520607e1b2aebd2973cc7defc31071e51e
Teemu Matvejeff <teemu.matvejeff@siili.com>
2019-10-04 14:46:13 +0300

    Lisäys tehtävään 4.2
    app.js - tiedoston luominen jäi vahingossa tekemättä aiemmin. Tämä puutos korjattiin tässä kommitissa.

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const http = require('http');
-const express = require('express');
-
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const app = require('./app');
+const config = require('./utils/config');
 
 const server = http.createServer(app);
-const config = require('./utils/config');
-const blogsRouter = require('./controllers/blogs')
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api/blogs', blogsRouter);
 
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
